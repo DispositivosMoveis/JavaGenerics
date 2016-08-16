94e1d787d98bd89e9ac0625db5c7d58fda1c4604
welder.souza <welder.souza@cnj.jus.br>
2016-08-16 16:31:22 -0300

    Adição de corpo na classe principalDois

--- src/br/edu/principal/PrincipalDois.java
+++ src/br/edu/principal/PrincipalDois.java
@@ -1,10 +1,31 @@
 package br.edu.principal;
 
+import br.edu.pacoteUm.Pessoa;
+import br.edu.pacoteUm.PessoaList;
+
 public class PrincipalDois {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		PessoaList<Pessoa<String>> pessoasList = new PessoaList<Pessoa<String>>();
+		
+		/**
+		 * Adicionando duas pessoas na listagem
+		 */
+		Pessoa<String> pessoa1 = new Pessoa<String>();
+		
+		pessoa1.setInfo("Ciclano");
+		
+		pessoasList.addPessoa(pessoa1);
 
+		Pessoa<String> pessoa2 = new Pessoa<String>();
+		
+		pessoa2.setInfo("Fulano");
+		
+		pessoasList.addPessoa(pessoa2);		
+		
+		pessoasList.addPessoa(pessoa2);
+		
 	}
 
 }
