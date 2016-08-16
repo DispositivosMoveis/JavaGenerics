6b643aec2cf2cd2ebd79c3ce1c95e3868d931acd
welder.souza <welder.souza@cnj.jus.br>
2016-08-16 16:46:17 -0300

    JavaDoc dos valores dos generics

--- src/br/edu/pacoteUm/PessoaList.java
+++ src/br/edu/pacoteUm/PessoaList.java
@@ -2,6 +2,21 @@ package br.edu.pacoteUm;
 
 import java.util.HashMap;
 
+/**
+ * Generics usados no Java
+ * 
+ * 	E - Elements
+ *  K - Keys
+ *  N - Numbers
+ *  T - Type
+ *  V - Value
+ * 
+ * @author welder
+ *
+ * @param <T>
+ */
+
+
 public class PessoaList<T extends Pessoa<String>> {
 
 	private HashMap<String,T> pessoas = new HashMap<String,T>();
